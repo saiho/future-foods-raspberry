8e7374d8eb21ff71fe8b671cfde1b5a4050f0b53
saiho <16374397+saiho@users.noreply.github.com>
2024-12-16 22:29:57 +0100

    Minor fix calling rsync
    Include hidden files, if necessary

--- install/upgrade_app.sh
+++ install/upgrade_app.sh
@@ -5,7 +5,7 @@ root_dir=$(dirname "$0")/..
 ssh pi@raspberrypi.local sudo systemctl stop future-foods-sensor
 ssh pi@raspberrypi.local sudo mount -o remount,rw /app
 ssh pi@raspberrypi.local rm -rf /app/future-foods/lib /app/future-foods/__pycache__
-rsync -rlt --progress $root_dir/app/* --exclude=__pycache__/ pi@raspberrypi.local:/app/future-foods
+rsync -rlt --progress $root_dir/app/ --exclude=__pycache__/ pi@raspberrypi.local:/app/future-foods
 ssh pi@raspberrypi.local py3compile /app/future-foods
 ssh pi@raspberrypi.local sudo mount -o remount,ro /app
 
